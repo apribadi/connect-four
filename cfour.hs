d12deceaa72c6e4b54cab50250b2599764657254
Aaron Pribadi <aaron.pribadi@gmail.com>
2011-12-20 23:58:45 -0800

    Made UI a little nicer.

--- cfour.hs
+++ cfour.hs
@@ -5,7 +5,11 @@ module Main where
 import Data.Array 
 import Data.List
 import Data.Maybe
+import System.IO
 
+-- Data definitions
+
+-- Slots can be empty, or can have player one or player two
 data Player  = Empty | One | Two deriving (Eq, Ord)
 
 instance Show Player where
@@ -16,32 +20,38 @@ instance Show Player where
 other One = Two
 other Two = One
 
+
+-- A board is a rectangular space with slots.  We also record what regions of
+-- four will result in a win.
+
 data Board = Board Int Int (Array Int Player) [[(Int, Int)]]
 
--- Constructor
 blank rn cn = Board rn cn xs (regions rn cn)
   where xs = listArray (0, rn*cn-1) (replicate (rn*cn) Empty)
 
 instance Show Board where
-  show b = unlines (map srow (reverse [0..rn-1])) ++ nums
+  show b = unlines (map srow (reverse [0..rn-1])) ++ line ++ "\n" ++ nums
     where Board rn cn xs regs = b
           row r = map (\c -> slot b r c) [0..cn-1]
           srow r = "|" ++ (intercalate "|" $ map show $ row r) ++ "|"
           nums = " " ++ (intercalate " " $ map show [0..cn-1]) ++ " "
+          line = concat $ replicate (cn*2+1) "*"
       
--- slot access
+-- Raw slot read/write
 slot b r c = xs ! ((r * cn) + c)                           
   where Board rn cn xs regs = b
 slotput b r c v = Board rn cn (xs // [((r*cn)+c, v)]) regs 
   where Board rn cn xs regs = b
 
--- validation
+-- Checks
 bbounds b = (rn, cn) where Board rn cn _ _ = b
 validcol b c = (0 <= c) && (c < cn) where (_, cn) = bbounds b
+
+-- Where can we place a piece?
 available b = filter (\c -> Empty == slot b (rn-1) c) [0..cn-1]
   where (rn, cn) = bbounds b
 
--- place a piece in a column
+-- Place a piece in a column.
 safeput :: Board -> Int -> Player -> Maybe Board
 safeput b c v
   | validcol b c && isJust mr = Just $ slotput b (fromJust mr) c v
@@ -49,7 +59,7 @@ safeput b c v
   where (rn, _) = bbounds b
         mr = find (\r -> Empty == slot b r c) [0..rn-1]
 
--- groups of four
+-- The regions of four on the board.
 regions :: Int -> Int -> [[(Int, Int)]]
 regions rn cn = let
     horiz r c = [(r, c + i) | i <- [0..3]]
@@ -66,8 +76,9 @@ regions rn cn = let
 
 
 -- minimax AI
---   TODO: add pruning
+--   TODO: add pruning?
 
+-- Does the board have a winner?
 winner b 
   | haswin One = Just One
   | haswin Two = Just Two
@@ -75,13 +86,14 @@ winner b
   where Board rn cn xs regs = b
         wonhere pl reg = all (\(r, c) -> pl == slot b r c) reg
         haswin pl = any (\reg -> wonhere pl reg) regs
-iswon b = isJust $ winner b
+finished b = isJust $ winner b
 
 count y xs = foldl' (\n x -> if x == y then n + 1 else n) 0 xs
 
--- won't overflow Int
+-- Large value that won't overflow Int.
 infty  = 2^28
 
+-- Heuristic scoring function on a board, with respect to a player.
 scoreboard b pl
   | (w == Just pl) = infty
   | (w == Just (other pl)) = - infty
@@ -90,43 +102,46 @@ scoreboard b pl
     Board _ _ _ regs = b
     w = winner b
     scorereg reg 
-      | s1 > 0  && s2 == 0 = s1*s1
-      | s1 == 0 && s2 > 0  = -s2*s2
+      | s1 > 0  && s2 == 0 = s1^3
+      | s1 == 0 && s2 > 0  = - s2^3
       | otherwise          = 0
       where
         vals = [slot b r c | (r, c) <- reg]
         s1 = count pl vals
         s2 = count (other pl) vals
 
+-- Recursive minimax algorithm.
 minimax :: Board -> Int -> Player -> (Int, [Int])
 minimax b depth pl 
-  | depth == 0 || iswon b = (scoreboard b pl, [])
+  | depth == 0 || finished b = (scoreboard b pl, [])
   | otherwise             = maximum [movec c | c <- available b]
   where
-    pl' = other pl
     movec c = (-n, c:cs)
       where
         b' = fromJust $ safeput b c pl
-        (n, cs) = minimax b' (depth - 1) pl'
+        (n, cs) = minimax b' (depth - 1) (other pl)
 
 -- interaction
 
 type GameState = (Board, Player)
+type Controller = GameState -> IO Int
 
-play :: (GameState -> IO Int) -> (GameState -> IO Int) -> GameState -> IO ()
-play f g s = let (b, turn) = s in 
-  if iswon b then 
+play :: Controller -> Controller -> GameState -> IO ()
+play f g s = let (b, pl) = s in 
+  if finished b then do
+    putStrLn ""
     putStrLn (show b) >> putStrLn "Game over."
   else do
-    putStrLn $ "It is the " ++ show turn ++ "'s turn."
+    putStrLn ""
+    putStrLn $ "It is the " ++ show pl ++ "'s turn."
     putStrLn $ show b
-    c <- case turn of One -> f s; Two -> g s
-    case safeput b c turn of
-      Nothing -> play f g s
-      Just b' -> play f g (b', other turn)
+    c <- case pl of One -> f s; Two -> g s
+    case safeput b c pl of
+      Nothing -> putStrLn "Invalid command\n" >> play f g s
+      Just b' -> play f g (b', other pl)
 
 inputcol (b, _) = do
-  putStrLn "Enter a column:" >> readcol
+  putStr "Enter a column: " >> hFlush stdout >> readcol
   where 
     readcol = do
       s <- getLine
